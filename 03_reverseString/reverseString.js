8a0d92e4915ba85decd32779ba0f7a0e0215caef
domagojrac <domagojradovcic1@gmail.com>
2022-09-22 18:44:14 +0200

    complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let finalString = ""
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        finalString = finalString + stringArray[i];
+    }
+    return finalString;
 };
 
 // Do not edit below this line
