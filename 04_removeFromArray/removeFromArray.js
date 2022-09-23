581eb8b80d53f728ba39dcb516ee2147328407bf
domagojrac <domagojradovcic1@gmail.com>
2022-09-23 14:27:42 +0200

    complete exercise 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
+    for (let i = 1; i <= arguments.length; i++) {
+        for (let j = 0; j < arguments[0].length; j++) {
+            if (arguments[i] === arguments[0][j]) {
+                arguments[0].splice(j, 1);
+            }
+        }
+    }
+    return arguments[0];
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
 const sumAll = function() {
+    let sum = 0
+    if (typeof(arguments[0]) === "number" && typeof(arguments[1]) ==="number" && arguments[0] >= 0 && arguments[1] >= 0) {
+        if (arguments[1] < arguments[0]) {
+            for (let i = arguments[1]; i <= arguments[0]; i++) {
+                sum = sum + i;
+            }
+            return sum;
+        }
+        else {
+            for (let i = arguments[0]; i <= arguments[1]; i++) {
+                sum = sum + i;
+            }
+            return sum;
+        }
+    }
 
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
