1dfaf8fd5a066766a206bf2811c7b2ccc616961f
domagojrac <domagojradovcic1@gmail.com>
2022-10-18 13:12:10 +0200

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    
+    if (num < 0) {
+        return "OOPS";
+    };
 
+    fibonacciSequence = [1, 1];
+
+    for (let i = 2; i < num; i++) {
+        fibonacciSequence.push(fibonacciSequence[i - 2] + fibonacciSequence[i - 1]);
+    };
+
+    return fibonacciSequence[num - 1];
 };
 
 // Do not edit below this line
