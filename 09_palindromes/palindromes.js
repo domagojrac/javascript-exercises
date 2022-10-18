bb97cd7b11900b97bec36087538daf44d992a53e
domagojrac <domagojradovcic1@gmail.com>
2022-10-18 12:47:55 +0200

    complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+const palindromes = function (userString) {
+    palindromeString = "";
+    userStringConverted = "";
 
+    for (let i = userString.length - 1; i >= 0; i--) {
+        if (userString.charAt(i).toLowerCase() == userString.charAt(i).toUpperCase()) {
+            continue;
+        } else {
+            palindromeString += userString.charAt(i).toLowerCase();
+        };
+    };
+
+    for (let i = 0; i < userString.length; i++) {
+        if (userString.charAt(i).toLowerCase() == userString.charAt(i).toUpperCase()) {
+            continue;
+        } else {
+            userStringConverted += userString.charAt(i).toLowerCase();
+        };
+    };
+
+
+    if (palindromeString == userStringConverted) {
+        return true;
+    } else {
+        return false;
+    }; 
 };
 
 // Do not edit below this line
