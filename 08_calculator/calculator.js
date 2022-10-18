ee3f471a030229d34ed93e5e37411ee738aaed39
domagojrac <domagojradovcic1@gmail.com>
2022-10-18 11:54:25 +0200

    complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,60 @@
 const add = function() {
-	
+  var sum = 0;
+
+  for (var i = 0, j = arguments.length; i < j; i++) {
+    sum += arguments[i];
+  };
+
+  return sum;
 };
 
 const subtract = function() {
-	
+	var sum = arguments[0];
+
+  for (var i = 1, j = arguments.length; i < j; i++) {
+    sum -= arguments[i];
+  };
+
+  return sum;
 };
 
 const sum = function() {
-	
+	var sum = 0;
+
+  for (var i = 0, j = arguments[0].length; i < j; i++) {
+    sum += arguments[0][i];
+  };
+
+  return sum;
 };
 
 const multiply = function() {
+  var sum = 1;
 
+  for (var i = 0, j = arguments[0].length; i < j; i++) {
+    sum *= arguments[0][i];
+  };
+
+  return sum;
 };
 
 const power = function() {
-	
+    var num = arguments[0];
+  
+    for (var i = 1, j = arguments.length; i < j; i++) {
+      num **= arguments[i];
+    };
+  
+    return num;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  var sum = 1;
+  for (let i = num; i > 1; i--) {
+    sum *= i;
+  }
+
+  return sum;
 	
 };
 
